e4d5234093a22f6c09a58bf14a248972d3daaecf
Peter Miller <pmiller@biscit.com.au>
2024-08-31 22:19:18 +0930

    use google photos and observer intersection for headings

---
+++ scripts.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', resizeHeadings);
+
+function resizeHeadings() {
+    const sections = document.querySelectorAll('.parallax-section');
+    const menuLinks = document.querySelectorAll('.menu a');
+
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            const id = entry.target.getAttribute('id');
+            const menuLink = document.querySelector(`.menu a[href="#${id}"]`);
+            
+            if (entry.isIntersecting) {
+                menuLinks.forEach(link => link.classList.remove('active'));
+                menuLink.classList.add('active');
+            }
+        });
+    }, {
+        threshold: 0.6 // Trigger when 60% of the section is in view
+    });
+
+    sections.forEach(section => {
+        observer.observe(section);
+    });
+}
