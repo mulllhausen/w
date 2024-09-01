6cd2e797b82d6ff3da431d25536cb034b16076ac
Peter Miller <pmiller@biscit.com.au>
2024-09-01 22:59:11 +0930

    clean up

--- scrollmagic-scripts.js
+++ scrollmagic-scripts.js
@@ -1,21 +1,18 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Initialize ScrollMagic Controller
+const sectionImages = {
+    'home': 'https://lh3.googleusercontent.com/pw/AP1GczNqCR-5qzTOoHOHItmJ_oyCJMos6X4Ik56X84JOim0wFR9Nhki6rbKnaoUgLs91pnHvlvNRZzDc7MgdQ2iIewyQQGToFga0D77-8qPPa7PA-r49bsK-wYQ-YPI_fm0eUk-INP7MTym5uv98rRWNG_rI=w1245-h934-s-no-gm?authuser=0',
+    'how-to-get-there': 'https://lh3.googleusercontent.com/pw/AP1GczP8qdFrU4hmAYO85zXpO7MQaPcCMv8d-wyXCBfEN5TL7OlPsXFQd7LQXoRoevFlfDq21yMtWe7kAquCbmOC27AlLLEYe6M5cUJzgQ_et16SHcbYou_YIZ9VGbP7ljOojk9UO_-6N01qpVGyD8PGa1cJ=w1245-h934-s-no-gm?authuser=0',
+    'rsvp': 'https://lh3.googleusercontent.com/pw/AP1GczPorK6_fE_b-tcABvo48-G3J4rpC8V-DVy3bluacZx8m_QxR1LQSCNdt5Q8UhkA7MkL_Z4Rtbe1-ZLp2I8oB3o7Uwdu4WIh9Uw6Q7aO8epB6yfPNF-NwkcV8As23VfLj03l7QHVedx81I0EKGafNI2k=w1245-h934-s-no-gm?authuser=0'
+};
+document.addEventListener('DOMContentLoaded', () => {
     const controller = new ScrollMagic.Controller();
-
-    // Define the background images for each section
-    const sectionImages = {
-        'home': 'https://lh3.googleusercontent.com/pw/AP1GczNqCR-5qzTOoHOHItmJ_oyCJMos6X4Ik56X84JOim0wFR9Nhki6rbKnaoUgLs91pnHvlvNRZzDc7MgdQ2iIewyQQGToFga0D77-8qPPa7PA-r49bsK-wYQ-YPI_fm0eUk-INP7MTym5uv98rRWNG_rI=w1245-h934-s-no-gm?authuser=0',
-        'how-to-get-there': 'https://lh3.googleusercontent.com/pw/AP1GczP8qdFrU4hmAYO85zXpO7MQaPcCMv8d-wyXCBfEN5TL7OlPsXFQd7LQXoRoevFlfDq21yMtWe7kAquCbmOC27AlLLEYe6M5cUJzgQ_et16SHcbYou_YIZ9VGbP7ljOojk9UO_-6N01qpVGyD8PGa1cJ=w1245-h934-s-no-gm?authuser=0',
-        'rsvp': 'https://lh3.googleusercontent.com/pw/AP1GczPorK6_fE_b-tcABvo48-G3J4rpC8V-DVy3bluacZx8m_QxR1LQSCNdt5Q8UhkA7MkL_Z4Rtbe1-ZLp2I8oB3o7Uwdu4WIh9Uw6Q7aO8epB6yfPNF-NwkcV8As23VfLj03l7QHVedx81I0EKGafNI2k=w1245-h934-s-no-gm?authuser=0'
-    };
     // Create scenes for each parallax section
     Object.keys(sectionImages).forEach(sectionId => {
         new ScrollMagic.Scene({
-            triggerElement: `#${sectionId}`, // The section to trigger the background change
-            duration: '50%', // The effect lasts throughout the entire section
-            triggerHook: 1  // The effect triggers as soon as the section enters the viewport
+            triggerElement: `#${sectionId}`,
+            duration: '50%',
+            triggerHook: 1
         })
-            .setClassToggle(`#${sectionId}`, 'active') // Optional: add a class for styling
+            .setClassToggle(`#${sectionId}`, 'active')
             .on('enter', () => {
                 document.getElementById('parallaxImage').style.backgroundImage =
                     `url('${sectionImages[sectionId]}')`;
@@ -31,4 +28,32 @@ document.addEventListener('DOMContentLoaded', function () {
             })
             .addTo(controller);
     });
+
+    Array
+        .from(document.querySelectorAll('.parallax-section, .content-section'))
+        .forEach(section => {
+            const menuID = section.dataset.menu;
+            if (!menuID) return;
+            new ScrollMagic.Scene({
+                triggerElement: section,
+                triggerHook: 0.1,
+                duration: section.clientHeight
+            })
+                .on('enter', () => {
+                    document
+                        .querySelectorAll('.menu a')
+                        .forEach(link => link.classList.remove('active'));
+                    document
+                        .querySelector(`.menu a[href="#${menuID}"]`)
+                        .classList
+                        .add('active');
+                })
+                .on('leave', () => {
+                    document
+                        .querySelector(`.menu a[href="#${menuID}"]`)
+                        .classList
+                        .remove('active');
+                })
+                .addTo(controller);
+        });
 });
